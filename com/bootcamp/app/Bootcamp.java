2623c363e08677fdd164f7030fd93f1e2a907711
blackhathacker101 <justindouglas1738@gmail.com>
2021-09-22 18:17:08 -0300

    update

--- com/bootcamp/app/Bootcamp.java
+++ com/bootcamp/app/Bootcamp.java
@@ -1,24 +1,18 @@
 package com.bootcamp.app;
 
-import java.io.File;
+import java.io.FileWriter;
 
 public class Bootcamp {
-
-	public static void main(String[] args) {
-		createTextFile();
+	public static void main(String[] argv) throws Exception {
+		FileWriter writer = new FileWriter("C:\\\\Tools\\\\workspace\\\\File\\\\src\\\\com\\\\bootcamp\\\\app\\\\Names.txt");
+		
+		String names[] = { "justin\n", "bryan\n", "juscelino\n", "enverny\n", "kirsten\n", "jianti\n" };
+		
+		int len = names.length;
+		
+		for (int i = 0; i < len; i++) {
+			writer.write(names[i]);
+		}
+		writer.close();
 	}
-	
-	private static void createTextFile() {
-
-		textFile = new File("mijntest.txt");
-	}
-
-	private static File textFile = new File("");
-	private String[] names = {"justin", 
-			"bryan", 
-			"juscelino", 
-			"enverny", 
-			"kirsten", 
-			"jianti"};
-	
 }
--- com/bootcamp/app/WriteFile.java
+++ com/bootcamp/app/WriteFile.java
@@ -1,18 +1,19 @@
-package com.bootcamp.app;
-import java.io.*;
-
-public class WriteFile {
-
-	public static void main(String[] args) {
-		
-		try {
-			FileWriter writeTo = new FileWriter("C:\\Tools\\workspace\\File\\src\\com\\bootcamp\\app\\File.txt");
-			writeTo.write("justin");
-			writeTo.close();
-			System.out.println("succesfuly written");
-		} catch (IOException e) {
-			System.out.println("error occured");
-			e.printStackTrace();
-		}
-	}
-}
+//package com.bootcamp.app;
+//
+//import java.io.*;
+//
+//public class WriteFile {
+//
+//	public static void main(String[] args) {
+//		String[] names = { "justin", "bryan", "juscelino", "enverny", "kirsten", "jianti" };
+//		try {
+//			FileWriter writeTo = new FileWriter("C:\\Tools\\workspace\\File\\src\\com\\bootcamp\\app\\File.txt");
+//			writeTo.write(names);
+//			writeTo.close();
+//			System.out.println("succesfuly written");
+//		} catch (IOException e) {
+//			System.out.println("error occured");
+//			e.printStackTrace();
+//		}
+//	}
+//}
