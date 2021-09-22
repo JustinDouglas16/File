2bb8dd60fcefc3985f5aa7301505e3dea21d912d
blackhathacker101 <justindouglas1738@gmail.com>
2021-09-22 15:12:07 -0300

    update

--- com/bootcamp/app/Bootcamp.java
+++ com/bootcamp/app/Bootcamp.java
@@ -1,17 +1,24 @@
 package com.bootcamp.app;
 
+import java.io.File;
+
 public class Bootcamp {
 
 	public static void main(String[] args) {
-		
+		createTextFile();
 	}
 	
+	private static void createTextFile() {
+
+		textFile = new File("mijntest.txt");
+	}
+
+	private static File textFile = new File("");
 	private String[] names = {"justin", 
 			"bryan", 
 			"juscelino", 
 			"enverny", 
 			"kirsten", 
 			"jianti"};
-	
 	
 }
