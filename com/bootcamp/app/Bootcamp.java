342427643c2228037d39bcde7e3fe1215ac8c3ad
blackhathacker101 <justindouglas1738@gmail.com>
2021-09-22 18:38:33 -0300

    update

--- com/bootcamp/app/Bootcamp.java
+++ com/bootcamp/app/Bootcamp.java
@@ -3,13 +3,14 @@ package com.bootcamp.app;
 import java.io.FileWriter;
 
 public class Bootcamp {
-	public static void main(String[] argv) throws Exception {
-		FileWriter writer = new FileWriter("C:\\\\Tools\\\\workspace\\\\File\\\\src\\\\com\\\\bootcamp\\\\app\\\\Names.txt");
-		
+	public static void main(String[] arg) throws Exception {
+		FileWriter writer = new FileWriter(
+				"C:\\\\Tools\\\\workspace\\\\File\\\\src\\\\com\\\\bootcamp\\\\app\\\\Names.txt");
+
 		String names[] = { "justin\n", "bryan\n", "juscelino\n", "enverny\n", "kirsten\n", "jianti\n" };
-		
+
 		int len = names.length;
-		
+
 		for (int i = 0; i < len; i++) {
 			writer.write(names[i]);
 		}
